5a73490f467ddf03ad8f668f94e75c65371e2314
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-07-14 22:03:37 -0400

    ch2

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -15,29 +15,29 @@ class BackPropagationNetwork:
 		for (l1, l2) in zip(layerSize[:1], layerSize[1:]):
 			self.weights.append(np.random.normal(scale=0.1, size = (l2, l1+1)))
 
-		def Run(self, input):
-			lnCases = input.shape[0]
+	def Run(self, input):
+		lnCases = input.shape[0]
 
-			self._layerInput = []
-			self._layerOutput = []
+		self._layerInput = []
+		self._layerOutput = []
 
-			for index in range(self.layerCount):
-				if index == 0:
-					layerInput - self.weights[0].dot(np.vstack([input.T, np.ones([1, lnCases])]))
-				else:
-					layerInput = self.weights[index].dot(np.vstack([self._layerOutput[-1], np.ones([1, lnCases])]))
-				
-				self._layerInput.append(layerInput)
-				self._layerOutput.append(self.sgm(layerInput))
-
-			return self._layerOutput[-1].T
-
-		def sgm(self, x, Derivative=False):
-			if not Derivative:
-				return 1 / (1+np>exp(-x))
+		for index in range(self.layerCount):
+			if index == 0:
+				layerInput = self.weights[0].dot(np.vstack([input.T, np.ones([1, lnCases])]))
 			else:
-				out = self.sgm(x)
-				return out*(1-out)
+				layerInput = self.weights[index].dot(np.vstack([self._layerOutput[-1], np.ones([1, lnCases])]))
+			
+			self._layerInput.append(layerInput)
+			self._layerOutput.append(self.sgm(layerInput))
+
+		return self._layerOutput[-1].T
+
+	def sgm(self, x, Derivative=False):
+		if not Derivative:
+			return 1 / (1+np.exp(-x))
+		else:
+			out = self.sgm(x)
+			return out*(1-out)
 
 
 if __name__ == "__main__": 
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 	print bpn.shape
 	print bpn.weights
 
-	lvInput = np.array([[0, 0], [1, 1], [-1, 0.5]])
+	lvInput = np.array([[0, 0], [1, 1]])
 	lvOutput = bpn.Run(lvInput)
 
 	print("Input: {0}\nOutput: {1}".format(lvInput, lvOutput))
