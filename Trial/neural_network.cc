9aebb7f2563ba573bd5ed44090382c2b77b94ca1
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-07-18 00:39:07 -0400

    outputting json

--- Trial/neural_network.cc
+++ Trial/neural_network.cc
@@ -29,21 +29,27 @@ std::vector<double> compute(std::vector<double> inputs) {
 }
 
 void NeuralNetwork::printNetwork() {
+	std::cout << "{";
 	for (int layer = 0; layer < neural_network.size(); layer++) {
-		std::cout << "{";
-		std::cout << layer << ":{";
+		std::cout << "\"" << layer << "\"" << ":{";
 		for (int neuron = 0; neuron < neural_network[0].size(); neuron++) {
-			std::cout << neuron << ":{";
+			std::cout << "\"" << neuron << "\"" << ":{";
 			std::vector<Neuron*> neurons = neural_network[layer];
 			for (int coefficients = 0; coefficients < neurons[neuron] -> coefficients.size(); coefficients++) {
-				std::cout << coefficients << ":" << neurons[neuron] -> coefficients[coefficients];
+				std::cout << "\"" << coefficients << "\"" << ":" << neurons[neuron] -> coefficients[coefficients];
 				if (coefficients != neurons[neuron] -> coefficients.size() - 1) {
 					std::cout << ",";
 				}
 			}
-			std::cout << "},";
+			std::cout << "}";
+			if (neuron != neural_network[0].size() - 1) {
+				std::cout << ",";
+			}
 		}
-		std::cout << "},";
+		std::cout << "}";
+		if (layer != neural_network.size() - 1) {
+			std::cout << ",";
+		}
 	}
-	std::cout <<  "},";
+	std::cout <<  "}";
 }
